883da19845a4fe3e29cd5486719edfd968d5f470
Vinci10 <seba.snk80@gmail.com>
2017-05-25 11:50:51 +0200

    Add files via upload

--- cinemaWeb/src/main/java/pl/cinema/service/UserServiceImpl.java
+++ cinemaWeb/src/main/java/pl/cinema/service/UserServiceImpl.java
@@ -1,16 +1,21 @@
 package pl.cinema.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import pl.cinema.model.Reservation;
 import pl.cinema.model.Role;
 import pl.cinema.model.User;
+import pl.cinema.repository.ReservationRepository;
 import pl.cinema.repository.RoleRepository;
 import pl.cinema.repository.UserRepository;
+import pl.cinema.repository.UserTicketRepository;
 
 @Service("userService")
 public class UserServiceImpl implements UserService{
@@ -21,6 +26,12 @@ public class UserServiceImpl implements UserService{
     private RoleRepository roleRepository;
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    private UserTicketRepository userTicketRepository;
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+
 
     @Override
     public User findUserByEmail(String email) {
@@ -39,5 +50,15 @@ public class UserServiceImpl implements UserService{
     @Override
     public void updatePremium(int y){
         userRepository.updatePremium(y);
+    }
+
+    @Override
+    public List<Reservation> userReservations(int id){
+        List <Integer> ticketsId = userTicketRepository.findTicketsId(id);
+        List <Reservation> reservations = new ArrayList<>();
+        for(Integer i : ticketsId){
+            reservations.add(reservationRepository.findReservation(i));
+        }
+        return reservations;
     }
 }
